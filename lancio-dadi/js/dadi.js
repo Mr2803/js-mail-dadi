6ca8759a87ce455b80a5e0073d7f2a3595bd0c12
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-21 20:04:47 +0200

    script "dadi" fix

--- lancio-dadi/js/dadi.js
+++ lancio-dadi/js/dadi.js
@@ -4,27 +4,19 @@
 var lancio1,lancio2
 
 //imposto condizioni per variabili primo lancio
-function dice1(){
+function dice(){
   lancio1 = Math.round(Math.random()*5+1);
-  document.getElementById('risultato-lancio1').innerHTML = lancio1
+  document.getElementById('risultato-lancio1').innerHTML = lancio1;
+  lancio2 = Math.round(Math.random()*5+1);
+  document.getElementById('risultato-lancio2').innerHTML = lancio2;
+  if(lancio1>lancio2){
+    document.getElementById('result').innerHTML = "vince giocatore 1";
+  }else if(lancio1<lancio2){
+    document.getElementById('result').innerHTML = "vince giocatore 2";
+  }else{
+    document.getElementById('result').innerHTML = "pareggio";
+  }
 }
 
 
 //imposto condizioni per variabili secondo lancio
-function dice2(){
-  lancio2 = Math.round(Math.random()*5+1);
-  document.getElementById('risultato-lancio2').innerHTML = lancio2
-}
-
-//imposto le condizioni
-if(lancio1>lancio2){
-  console.log("Al giocatore 1 è uscito" + lancio1) // DEBUG
-  console.log("Al giocatore 2 è uscito" + lancio2) // DEBUG
-  console.log("Vince il giocatore 1")
-}else if(lancio1<lancio2){
-  console.log("Al giocatore 1 è uscito" + lancio1) // DEBUG
-  console.log("Al giocatore 2 è uscito" + lancio2) // DEBUG
-  console.log("Vince il giocatore 2")
-}else(
-  console.log("Avete pareggiato") // DEBUG
-)
