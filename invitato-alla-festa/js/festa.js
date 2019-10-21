6a1bba30f1e4c0c40752b1256058a1d1b0ed8b51
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-21 14:02:03 +0200

    fix error in "dadi" , create structure of "festa"

---
+++ invitato-alla-festa/js/festa.js
@@ -0,0 +1,7 @@
+//dichiarazione arrey
+var email =["email1@gmail.com","email1@gmail.com","email1@gmail.com","email1@gmail.com","email1@gmail.com"];
+console.log("mail valide : " + email)
+
+//chiedo all'utente di inserire la sua mail
+var emailuser = prompt("inserisci una mail");
+console.log("la mail inserita dall'utente è:" + emailuser)
--- lancio-dadi/js/dadi.js
+++ lancio-dadi/js/dadi.js
@@ -10,13 +10,13 @@ lancio2 = Math.round(Math.random()*5+1);
 
 //imposto le condizioni
 if(lancio1>lancio2){
-  console.log("Al giovatore 1 è uscito" + lancio1) // DEBUG
-  console.log("Al giovatore 2 è uscito" + lancio2) // DEBUG
+  console.log("Al giocatore 1 è uscito" + lancio1) // DEBUG
+  console.log("Al giocatore 2 è uscito" + lancio2) // DEBUG
   console.log("Vince il giocatore 1")
 }else if(lancio1<lancio2){
-  console.log("Al giovatore 1 è uscito" + lancio1) // DEBUG
-  console.log("Al giovatore 2 è uscito" + lancio2) // DEBUG
+  console.log("Al giocatore 1 è uscito" + lancio1) // DEBUG
+  console.log("Al giocatore 2 è uscito" + lancio2) // DEBUG
   console.log("Vince il giocatore 2")
 }else(
-  console.log("Avete pareggiato") // DEBUG 
+  console.log("Avete pareggiato") // DEBUG
 )
