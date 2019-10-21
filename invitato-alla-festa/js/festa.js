7dd90c2a77da8d4c9e629914d546d4ebf615131a
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-21 14:15:10 +0200

    i have create a virus...

--- invitato-alla-festa/js/festa.js
+++ invitato-alla-festa/js/festa.js
@@ -1,7 +1,14 @@
 //dichiarazione arrey
-var email =["email1@gmail.com","email1@gmail.com","email1@gmail.com","email1@gmail.com","email1@gmail.com"];
+var email =["email1@gmail.com","email2@gmail.com","email3@gmail.com","email4@gmail.com","email5@gmail.com"];
 console.log("mail valide : " + email)
 
 //chiedo all'utente di inserire la sua mail
 var emailuser = prompt("inserisci una mail");
 console.log("la mail inserita dall'utente è:" + emailuser)
+
+
+for (var i = emailuser; i !== email ; i++){
+  console.log("Attenzione la mail inserita non è valida");
+}
+
+console.log("complimenti puoi entrare alla festa")
