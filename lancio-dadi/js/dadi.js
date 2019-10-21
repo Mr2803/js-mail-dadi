b9ce0184cb35c5e10d4b97da9d88665b82705b72
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-22 00:22:00 +0200

    Modified script. Now the dice roll buttons are 2. At the end of the launch the name of the winner appears and a new button "Another game" that refreshes the page.

--- lancio-dadi/js/dadi.js
+++ lancio-dadi/js/dadi.js
@@ -6,7 +6,7 @@ var lancio1,lancio2
 //imposto la funzione che al click fa partire lo script del lancio dadi
 function dice(){
   //imposto il valore della variabile lancio1 con math-random
-  lancio1 = Math.round(Math.random()*5+1);
+  lancio1 = Math.floor(Math.random()*6 +1);
   if (lancio1==1) {
     document.getElementById('risultato-lancio1').innerHTML= "<img src='img/face1.png'>";
   }else if (lancio1==2){
@@ -20,31 +20,39 @@ function dice(){
   }else if (lancio1==6){
     document.getElementById('risultato-lancio1').innerHTML= "<img src='img/face6.png'>";
   }
+console.log(lancio1)
 
-  //imposto il valore della variabile lancio2 con math-random
-  lancio2 = Math.round(Math.random()*5+1);
-  if (lancio2==1) {
-    document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face1.png'>";
-  }else if (lancio2==2){
-    document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face2.png'>";
-  }else if (lancio2==3){
-    document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face3.png'>";
-  }else if (lancio2==4){
-    document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face4.png'>";
-  }else if (lancio2==5){
-    document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face5.png'>";
-  }else if (lancio2==6){
-    document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face6.png'>";
-  }
-
-  if(lancio1>lancio2){
-    document.getElementById('result').innerHTML = "Vince giocatore 1";
-  }else if(lancio1<lancio2){
-    document.getElementById('result').innerHTML = "Vince giocatore 2";
-  }else{
-    document.getElementById('result').innerHTML = "Avete pareggiato";
-  }
 }
 
+function dice2(){
+lancio2 = Math.floor(Math.random()*6 +1);
+if (lancio2==1) {
+  document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face1.png'>";
+}else if (lancio2==2){
+  document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face2.png'>";
+}else if (lancio2==3){
+  document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face3.png'>";
+}else if (lancio2==4){
+  document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face4.png'>";
+}else if (lancio2==5){
+  document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face5.png'>";
+}else if (lancio2==6){
+  document.getElementById('risultato-lancio2').innerHTML= "<img src='img/face6.png'>";
+}
+if(lancio1>lancio2){
+  document.getElementById('result').innerHTML = "Vince il giocatore 1";
+  document.getElementById('reset').innerHTML = "Un'altra partita?";
+}else if(lancio1<lancio2){
+  document.getElementById('result').innerHTML = "Vince il giocatore 2";
+  document.getElementById('reset').innerHTML = "Un'altra partita?";
+}else{
+  document.getElementById('result').innerHTML = "Avete pareggiato";
+  document.getElementById('reset').innerHTML = "Un'altra partita?";
+}
+}
+
+
+
+console.log(lancio2)
 
 //imposto condizioni per variabili secondo lancio
