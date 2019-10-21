bbd79e3367c464ba01fd7a3f797ddc43f00b44ca
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-21 13:51:08 +0200

    add comments and debug

--- lancio-dadi/js/dadi.js
+++ lancio-dadi/js/dadi.js
@@ -1,18 +1,22 @@
 //Gioco dei dadi, chi fa di più vince;
 
+//creo variabili
 var lancio1,lancio2
 
+//imposto condizioni per variabili primo lancio
 lancio1 = Math.round(Math.random()*5+1);
+//imposto condizioni per variabili secondo lancio
 lancio2 = Math.round(Math.random()*5+1);
 
+//imposto le condizioni
 if(lancio1>lancio2){
-  console.log("Al giovatore 1 è uscito" + lancio1)
-  console.log("Al giovatore 2 è uscito" + lancio2)
+  console.log("Al giovatore 1 è uscito" + lancio1) // DEBUG
+  console.log("Al giovatore 2 è uscito" + lancio2) // DEBUG
   console.log("Vince il giocatore 1")
 }else if(lancio1<lancio2){
-  console.log("Al giovatore 1 è uscito" + lancio1)
-  console.log("Al giovatore 2 è uscito" + lancio2)
+  console.log("Al giovatore 1 è uscito" + lancio1) // DEBUG
+  console.log("Al giovatore 2 è uscito" + lancio2) // DEBUG
   console.log("Vince il giocatore 2")
 }else(
-  console.log("Avete pareggiato")
+  console.log("Avete pareggiato") // DEBUG 
 )
