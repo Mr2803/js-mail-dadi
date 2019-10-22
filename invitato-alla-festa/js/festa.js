7d6ad66bc8598aa3f1223e8e03d22fe593456daf
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-22 12:20:01 +0200

    fix script

--- invitato-alla-festa/js/festa.js
+++ invitato-alla-festa/js/festa.js
@@ -3,17 +3,29 @@ var email =["email1@gmail.com","email2@gmail.com","email3@gmail.com","email4@gma
 console.log("mail valide : " + email);
 
 // chiedo all'utente di inserire la sua mail
-var emailUser = prompt("inserisci una mail");
-console.log("la mail inserita dall'utente è:" + emailUser);
+// var emailUser = prompt("inserisci una mail");
+// console.log("la mail inserita dall'utente è:" + emailUser);
 
 //imposto variabile di base
-var messaggioTrovato = document.getElementById('enterOrNot').innerHTML="Mi dispiace ma non sei invitato";
+// var messaggioTrovato = document.getElementById('enterOrNot').innerHTML="Mi dispiace ma non sei invitato";
 
 //imposto il ciclo
-for(i=0; i < email.length; i++){
-  //imposto condizione del ciclo
-  if(email[i] == emailUser){
-    messaggioTrovato= document.getElementById('enterOrNot').innerHTML="Benvenuto";
+// for(i=0; i < email.length; i++){
+//   //imposto condizione del ciclo
+//   if(email[i] == emailUser){
+//     messaggioTrovato= document.getElementById('enterOrNot').innerHTML="Benvenuto";
+//   }
+// }
+
+function insertMail(){
+  var emailUser = prompt("inserisci una mail");
+  var messaggioTrovato = document.getElementById('enterOrNot').innerHTML="Mi dispiace ma non sei invitato";
+  console.log("la mail inserita dall'utente è:" + emailUser);
+  for(i=0; i < email.length; i++){
+    //imposto condizione del ciclo
+    if(email[i] == emailUser){
+      messaggioTrovato= document.getElementById('enterOrNot').innerHTML="Benvenuto";
+    }
   }
 }
 
