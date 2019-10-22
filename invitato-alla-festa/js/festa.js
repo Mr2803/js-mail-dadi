2df28401ad1c211ba668bc37df413d1931e4006a
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-22 12:51:44 +0200

    fix script. Now if the inserted email is one of those accepted, it is inserted in the list.

--- invitato-alla-festa/js/festa.js
+++ invitato-alla-festa/js/festa.js
@@ -2,6 +2,20 @@
 var email =["email1@gmail.com","email2@gmail.com","email3@gmail.com","email4@gmail.com","email5@gmail.com"];
 console.log("mail valide : " + email);
 
+//imposto la funzione che al click fa partire il prompt che domanda la mail
+function insertMail(){
+  var emailUser = prompt("inserisci una mail");
+  var messaggioTrovato = document.getElementById('enterOrNot').innerHTML="Mi dispiace ma non sei invitato";
+  //imposto condizione del ciclo
+  for(i=0; i < email.length; i++){
+    if(email[i] == emailUser){
+      messaggioTrovato= document.getElementById('enterOrNot').innerHTML="Benvenuto";
+      document.getElementById('newmail').innerHTML= emailUser;
+    }
+  }
+}
+
+
 // chiedo all'utente di inserire la sua mail
 // var emailUser = prompt("inserisci una mail");
 // console.log("la mail inserita dall'utente è:" + emailUser);
@@ -16,19 +30,6 @@ console.log("mail valide : " + email);
 //     messaggioTrovato= document.getElementById('enterOrNot').innerHTML="Benvenuto";
 //   }
 // }
-
-function insertMail(){
-  var emailUser = prompt("inserisci una mail");
-  var messaggioTrovato = document.getElementById('enterOrNot').innerHTML="Mi dispiace ma non sei invitato";
-  console.log("la mail inserita dall'utente è:" + emailUser);
-  for(i=0; i < email.length; i++){
-    //imposto condizione del ciclo
-    if(email[i] == emailUser){
-      messaggioTrovato= document.getElementById('enterOrNot').innerHTML="Benvenuto";
-    }
-  }
-}
-
 
 
 // var messaggioTrovato = "non puoi entrare";
